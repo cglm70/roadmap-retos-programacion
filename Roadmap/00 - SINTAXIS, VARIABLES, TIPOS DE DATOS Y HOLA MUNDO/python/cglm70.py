fefc8b1bdb50284a471e7b17a58e9c8e04c4e013
cglm70 <cglm70@hotmail.com>
2024-10-10 17:54:05 +0200

    Create cglm70.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/cglm70.py
@@ -0,0 +1,36 @@
+# https://www.python.org/
+
+# Comentario en una línea
+
+"""
+Esto también
+es
+un comentario
+en varias líneas 
+"""
+
+'''
+Esta es una segunda forma
+de escribir un
+comentario que contenga
+varias líneas en python
+'''
+mi_primera_variable="mi variable"
+mi_primera_variable="mi variable nueva"
+
+MY_CONSTAND= "mi constante" # se usa las mayusculas por convención
+
+my_first_int= 1
+my_first_float=3.5
+my_first_bool=true
+my_second_bool=false
+my_string="mi cadena de texto" # con comillas doble
+my_other_string='mi otra cadena de texto' # con comillas simples
+print("¡Hola, Phyton!")
+
+print(type(my_first_int)
+print(type(my_first_float)
+print(type(my_first_bool)
+print(type(my_second_bool)
+print(type(my_string)
+
